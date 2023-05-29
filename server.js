205e39e121360032e262c2bd45ead88df7bf1925
ArronRobinson <arron_robinson50@hotmail.com>
2023-05-30 00:34:15 +0200

    init ejs

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
-const student = require("./modules/student")
-console.log(student);
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.set('view engine', 'ejs')
+
+const user = {
+    firstName: 'tim',
+    lastName: 'cook',
+}
+
+
+// app.use('/', express.static('views/pages'))
+app
+    .get('/', (req, res) => {
+        res.render("pages/index", {
+            user
+        })
+    })
+    .use((req, res) => {
+        res.status(404).sendFile(__dirname + '/views/pages/404.html');
+    })
+    .listen(port)
