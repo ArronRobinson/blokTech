8623612c303b419b31dcccde35c35989b77e46e9
ArronRobinson <arron_robinson50@hotmail.com>
2023-06-01 01:11:30 +0200

    password hashing

--- database.js
+++ database.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const uri = process.env.MONGO_URL;
 
-// Define the user schema
 const userSchema = new Schema({
   email: { type: String, required: true },
   password: { type: String, required: true }
 });
 
-// Create the user model based on the user schema
 const User = mongoose.model('User', userSchema);
 
 async function connect() {
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 3000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv')
+const bcrypt = require('bcrypt');
 dotenv.config()
 const {
     User,
@@ -13,29 +14,29 @@ const {
 connect()
 app.set('view engine', 'pug')
 
-const user = {
-    firstName: 'tim',
-    lastName: 'cook',
-}
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static('views/pages'))
 app
     .get('/', (req, res) => {
         res.render('pages', { title: 'Hey', message: 'Hello there!' })
-      })
+    })
     .get('/signup', (req, res) => {
         res.render('pages/signup', { title: 'Hey', message: 'Hello there!' })
-      })
+    })
     .post('/signup', async (req, res) => {
         const email = req.body.email;
-        const password = req.body.password;
-        const user = new User({
-            email, password
-        })
-        await user.save()
-        console.log(email, password)
-        res.status(200).send('yoooo')
+        const plainPassword = req.body.password;
+        bcrypt.hash(plainPassword, 10, async (err, hashedPassword) => {
+            if (err) {
+                console.error('Error hashing password:', err);
+                return res.status(500).send('nope');
+            }
+            const user = new User({
+                email, password: hashedPassword
+            })
+            await user.save()
+            res.status(200).send('yoooo')
+        });
     })
     .use((req, res) => {
         res.status(404).sendFile(__dirname + '/views/pages/404.html');
